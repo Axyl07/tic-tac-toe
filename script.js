146d959fc9c9903853ffc90e855e62a43afd2bd9
Axyl07 <aryanchauhan000777@gmail.com>
2024-11-15 18:39:14 +0530

    console version finished

--- script.js
+++ script.js
@@ -1,191 +1,150 @@
 
-// console.log('the array is empty add elements by adding to their locations as gameboard[i][j] run checkgameboard to check if someone wins');
 
-
-
-// function createGameboard() {
-//     const row = 3;
-//     const col = 3;
-//     const board = [];
-//     for (let i = 0; i < row; i++) {
-//         board[i] = [];
-//         for (let j = 0; j < col; j++) {
-//             board[i].push(0);
-//         }
-
-//     }
-//     // const getBoard = () => board;
-//     return board; 
-// }
-// console.log(createGameboard());
-
-
-const gameboard = function(){
-        const row = 3;
-        const col = 3;
-        const board = [];
-        for (let i = 0; i < row; i++) {
-            board[i] = [];
-            for (let j = 0; j < col; j++) {
-                board[i].push(0);
-            }
-    
-        }
-    console.log(typeof (board));
-    
-        const getBoard = () => board;
-        const addToken = (row,col,token) => {
-            board[row][col] = token;
-    }   
-    const getElement = (row,col) => {
-        return board[row][col];
+const gameboard = function () {
+  const row = 3;
+  const col = 3;
+  const board = [];
+  for (let i = 0; i < row; i++) {
+    board[i] = [];
+    for (let j = 0; j < col; j++) {
+      board[i].push('');
     }
-    const check = (row, col)=>{
-        let element = board[row][col];
-        if (row>=0 && row<=2 && col>=0 && col<=2 ) {
-            if (
-                element === board[row-1][col] && element ===[row+1][col]
-            ) {
-                
-            }
+  }
+
+
+  const getBoard = () => board;
+  const addToken = (row, col, token) => {
+    board[row][col] = token;
+  };
+  const getElement = (row, col) => {
+    return board[row][col];
+  };
+  const checkFill = () => {
+    
+      for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+          if (board[i][j] === undefined || board[i][j] === null || board[i][j]==='') {
+            return false; // array is not fully filled
+          }
         }
+      }
+      return true; // array is fully filled
+    
+  }
+  const check = () => {
+    function arrayEquals(a, b) {
+      if (a.length !== b.length) return false;
+      for (let i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) return false;
+      }
+      return true;
     }
-    // const wincondition = function(array){
-    //     let element = array[0];
-    //     let status;
-    //         if (element === array[1] && element === array[2]) {
-    //              status = true;
-    //                 let winningLine = array;
-    //                 return { element, status, winningLine };
-    //         } else {
-    //             status = false
-    //             return status;     
-    //             }
-    //     }
-    
-    
-    // function wincondition(gameboardLine) {
-    //     if (gameboardLine[0] !== '') {
-    //         let element = gameboardLine[0];
-    //         let status;
-    //         if (element === gameboardLine[1] && element === gameboardLine[2]) {
-    //              status = true;
-    //                 let winningLine = gameboardLine;
-    //                 return { element, status, winningLine };
-    //         } else {
-    //             status = false
-    //             return status;     
-    //             }
-    //     }
-            
-        
-    // }
-    // // console.log(wincondition(gameboard[2]));
-    // function check(board) {
-    //     const row0 = board[0];
-    //     const row1 = board[1];
-    //     const row2 = board[2];
-    //     const col0 = board.map(row => row[0]);
-    //     const col1 = board.map(row => row[1]);
-    //     const col2 = board.map(row => row[2]);
-    //     const diag0 = [];
-    //     diag0.push(board[0][0], board[1][1], board[2][2]);
-    //     const diag1 = [];
-    //     diag1.push(board[0][2], board[1][1], board[2][0]);
-    //     let result;
-    //     if ((result = wincondition(row0)).status) {
-    //         return  `Won on row0'${result.element}`;
-    //     }
-    //     if ((result=wincondition(row1)).status) {
-    //         return `Won on row1 with ${result.element}`;
-    //     }
-    //     if ((result=wincondition(row2)).status) {
-    //         return `Won on row2 with ${result.element}`;
-    //     }
-    //     if ((result = wincondition(col0)).status) {
-    //         return `Won on col0 with ${result.element}`;
-    //     }
-    //     if ((result = wincondition(col1)).status) {
-    //         return `Won on col1 with ${result.element}`;
-    //     }
-    //     if ((result = wincondition(col2)).status) {
-    //         return `Won on col2 with ${result.element}`
-    //     }
-    //     if ((result = wincondition(diag0)).status) {
-    //         return `Won on diag0 with ${result.element}`;
-    //     }
-    //     if ((result = wincondition(diag1)).status) {
-    //         return `Won on diag1 with ${result.element}`;
-    //     }
-    //     else {
-    //         return 'Tie'
-    //     }
-    
-    // }
+    let sampleX = ['x', 'x', 'x'];
+    let sampleO = ['o', 'o', 'o'];
+    //divide 2d matrix to 8 1d matrices to check for consecutive x or o's
+    let row0 = [];
+    row0.push(board[0][0], board[0][1], board[0][2]);
+    let row1 = [];
+    row1.push(board[1][0], board[1][1], board[1][2]);
+    let row2 = [];
+    row2.push(board[2][0], board[2][1], board[2][2]);
+    let col0 = [];
+    col0.push(board[0][0], board[1][0], board[2][0]);
+    let col1 = [];
+    col1.push(board[0][1], board[1][1], board[2][1]);
+    let col2 = [];
+    col2.push(board[0][2], board[1][2], board[2][2]);
+    let diag0 = [];
+    diag0.push(board[0][0], board[1][1], board[2][2]);
+    let diag1 = [];
+    diag1.push(board[0][2], board[1][1], board[2][0]);
+    //check conditions
+    if (arrayEquals(row0, sampleX)) {
+      return 'x';
+    } else if (arrayEquals(row0, sampleO)) return 'o';
+    else if (arrayEquals(row1, sampleX)) {
+      return 'x';
+    } else if (arrayEquals(row1, sampleO)) return 'o';
+    else if (arrayEquals(row2, sampleX)) {
+      return 'x';
+    } else if (arrayEquals(row2, sampleO)) return 'o';
+    else if (arrayEquals(col0, sampleX)) {
+      return 'x';
+    } else if (arrayEquals(col0, sampleO)) return 'o';
+    else if (arrayEquals(col1, sampleX)) {
+      return 'x';
+    } else if (arrayEquals(col1, sampleO)) return 'o';
+    else if (arrayEquals(col2, sampleX)) {
+      return 'x';
+    } else if (arrayEquals(col2, sampleO)) return 'o';
+    else if (arrayEquals(diag0, sampleX)) {
+      return 'x';
+    } else if (arrayEquals(diag0, sampleO)) return 'o';
+    else if (arrayEquals(diag1, sampleX)) {
+      return 'x';
+    } else if (arrayEquals(diag1, sampleO)) return 'o';
+    if (checkFill()) return 'tie';
+    return null;
 
 
-
-
-
-
-
-    return {getBoard,addToken,getElement}; 
-    }  
-
+ 
+  };
+  return { getBoard, addToken, getElement,check,checkFill };
+};
 
 function gameController(
-    playerOneName = "Player 1",
-    playerTwoName = "Player 2"
+  playerOneName = "Player 1",
+  playerTwoName = "Player 2"
 ) {
-    
-    const board = gameboard();
-    const players = [
-        {
-            name: playerOneName,
-            token: 'x'
-        },
-        {
-            name: playerTwoName,
-            token: 'o'
-        }
-    ]
-    let activePlayer = players[0];
-    const switchPlayerTurn = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    }
-    const getActivePlayer = () => activePlayer;
-    const printNewRound = () => {
-        console.log(board.getBoard());
-        console.log(`It's ${getActivePlayer().name}'s Turn,Enter coordinates where you want to place your token using 'game.playRound(row,col)'`);
-    };
-    const playRound = (row, col) => {
+  const board = gameboard();
+  const players = [
+    {
+      name: playerOneName,
+      token: "x",
+    },
+    {
+      name: playerTwoName,
+      token: "o",
+    },
+  ];
+  let activePlayer = players[0];
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+  const getActivePlayer = () => activePlayer;
+  const printNewRound = () => {
+    console.log(board.getBoard());
+    console.log(
+      `It's ${
+        getActivePlayer().name
+      }'s Turn,Enter coordinates where you want to place your token using 'game.playRound(row,col)'`
+    );
+  };
+  let count = 0;
+  const playRound = (row, col) => {
 
-            console.log(
-                `Dropping ${getActivePlayer().name}'s token into position ${row},${col}`
-            );
-        console.log(typeof (board));
-        board.addToken(row, col, getActivePlayer().token);
-        console.log(board.getElement(row,col));
-        // console.log(board.check());
-            switchPlayerTurn();
-            printNewRound();
-
-    };
-    printNewRound();
-    return {
-        playRound,
-        getActivePlayer
-    }
+    if (board.getElement(row, col) == '') {
+      count++;
+      console.log(
+        `Dropping ${getActivePlayer().name}'s token into position ${row},${col}`
+      );
+      board.addToken(row, col, getActivePlayer().token);
+     
+        if (board.check() == 'x') {
+          console.log('X won i.e Player 1 has won !!! Refresh to play again');
+        } else if (board.check() == 'o') {
+          console.log('O i.e. Player 2 has won!!! Refresh to play again');
+        } else if (board.check() == 'tie') console.log('The game tied Refresh to play again');
+  
+      switchPlayerTurn();
+      printNewRound();
+    } else console.log(`You can't override other players token lol(choose a different position(other than ${row},${col}))`);
+  };
+  printNewRound();
+  return {
+    playRound,
+    getActivePlayer,
+  };
 }
 
 const game = gameController();
-
-// console.log(gameboard.createGameboard());
-// let gameboard = gameboardObject.gameboard;
-// console.log('board is '+ gameboard );
-// console.log(gameboard[2]);
-// console.log(wincondition(gameboard[1])); 
-
-
-
-// console.log(check(gameboard));
